cc46a7c8f5d4007cc90016528cab53e4a75c510b
Janik Schumacher <janik.schumacher@helpline.pmcs.de>
2020-10-15 14:02:55 +0200

    added custom command

--- src/models/translation-merger.ts
+++ src/models/translation-merger.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-import { GahPlugin, GahPluginConfig } from '@awdware/gah-shared';
+import { GahModuleType, GahPlugin, GahPluginConfig } from '@awdware/gah-shared';
 
 import { TranslationManagerConfig } from './translation-manager-config';
 import { TranslationCollection } from './translation-collection';
@@ -27,7 +27,7 @@ export class TranslationMerger extends GahPlugin {
     // .*\.(\w+)\.json
     newCfg.localeRegexPattern = await this.promptService.input({
       msg: 'Please enter a regex that has one matching group that matches the locale of the filename. leave empty for matching the whole filename'
-        +  ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
+        + ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
       default: '',
       enabled: () => !(existingCfg?.localeRegexPattern),
     }) ?? existingCfg.localeRegexPattern;
@@ -41,46 +41,67 @@ export class TranslationMerger extends GahPlugin {
     return newCfg;
   }
 
+  registerCommands() {
+    this.registerCommandHandler('i18n', () => {
+      const moduleType = this.configurationService.getGahModuleType();
+      if (moduleType === GahModuleType.MODULE || GahModuleType.UNKNOWN) {
+        this.loggerService.error('This command can only be executed in gah host folders');
+        return false;
+      }
+      const name = this.fileSystemService.directoryName(process.cwd());
+      this.mergeTranslations(name);
+      return true;
+    });
+  }
+
   public onInit() {
+    this.registerCommands();
     this.registerEventListener('ASSETS_COPIED', (event) => {
-      const name = event.module?.isHost ? this.fileSystemService.directoryName(event.module.basePath) : event.module?.moduleName;
+      if (!event.module?.isHost) {
+        return;
+      }
+      const name = this.fileSystemService.directoryName(event.module.basePath);
+      this.mergeTranslations(name);
+    });
+  }
 
-      this.loggerService.log('Merging translation files for ' + name);
+  private mergeTranslations(name: string | undefined) {
+    this.loggerService.log(`Merging translation files for ${name}`);
 
-      const cfg = this.config as TranslationManagerConfig;
-      if (!cfg)
-        throw new Error('Plugin settings have not been provided.');
+    const cfg = this.config as TranslationManagerConfig;
+    if (!cfg) { throw new Error('Plugin settings have not been provided.'); }
 
-      if (!cfg.searchGlobPattern)
-        throw new Error('Missing Setting: searchGlobPattern');
-      if (!cfg.destinationPath)
-        throw new Error('Missing Setting: destinationPath');
+    if (!cfg.searchGlobPattern) { throw new Error('Missing Setting: searchGlobPattern'); }
+    if (!cfg.destinationPath) { throw new Error('Missing Setting: destinationPath'); }
 
-      const allTranslationFiles = this.fileSystemService.getFilesFromGlob('.gah/' + cfg.searchGlobPattern, ['node_modules'], true);
+    const allTranslationFiles = this.fileSystemService.getFilesFromGlob(`.gah/${cfg.searchGlobPattern}`, ['node_modules'], true);
 
-      this.loggerService.debug('Found translation files:' + allTranslationFiles.join(', '));
+    this.loggerService.debug(`Found translation files:${allTranslationFiles.join(', ')}`);
 
-      const translationCollection = new Array<TranslationCollection>();
+    const destinationPath = this.fileSystemService.join('.gah', cfg.destinationPath);
+    const translationCollection = new Array<TranslationCollection>();
 
-      allTranslationFiles.forEach(x => {
+    allTranslationFiles
+      .filter(x => !x.startsWith(destinationPath))
+      .forEach(x => {
         let locale: string;
         let prefix: string | undefined = undefined;
         if (cfg.localeRegexPattern) {
           const localeRegex = new RegExp(cfg.localeRegexPattern);
           const localeMatch = path.basename(x).match(localeRegex);
           if (!localeMatch || !(localeMatch?.[1])) {
-            throw new Error('The locale matcher did not fine the locale in the filename: ' + path.basename(x));
+            throw new Error(`The locale matcher did not find the locale in the filename: ${path.basename(x)}`);
           }
           locale = localeMatch[1];
 
-          if(cfg.prefixRegexPattern) {
+          if (cfg.prefixRegexPattern) {
             const prefixRegex = new RegExp(cfg.prefixRegexPattern);
             const prefixMatch = path.basename(x).match(prefixRegex);
             if (!prefixMatch || !(prefixMatch?.[1])) {
-              throw new Error('The prefix matcher did not fine the prefix in the filename: ' + path.basename(x));
+              throw new Error(`The prefix matcher did not find the prefix in the filename: ${path.basename(x)}`);
             }
             prefix = prefixMatch[1];
-  
+
           }
 
         } else {
@@ -97,20 +118,19 @@ export class TranslationMerger extends GahPlugin {
           translationCollection.push(trans);
         }
         const parsedContent = JSON.parse(content);
-        if(prefix) {
+        if (prefix) {
           trans.translations[prefix] = parsedContent;
         } else {
           trans.translations = { ...trans.translations, ...parsedContent };
         }
       });
 
-      this.fileSystemService.ensureDirectory(path.join('.gah', cfg.destinationPath));
+    this.fileSystemService.ensureDirectory(destinationPath);
 
-      translationCollection.forEach(x => {
-        const filePath = path.join('.gah', cfg.destinationPath, `${x.locale}.json`);
-        this.fileSystemService.saveObjectToFile(filePath, x.translations, true);
-      });
-      this.loggerService.success('Translation files merged successfully!');
+    translationCollection.forEach(x => {
+      const filePath = this.fileSystemService.join(destinationPath, `${x.locale}.json`);
+      this.fileSystemService.saveObjectToFile(filePath, x.translations, true);
     });
+    this.loggerService.success('Translation files merged successfully!');
   }
 }
