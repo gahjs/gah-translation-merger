aebac4624140d3337352bbfe4b77a775fee66b8d
Janik Schumacher <rndmb0t@gmail.com>
2021-05-17 20:35:51 +0200

    intermediate state of reporting mismatches in files

--- src/models/translation-merger.ts
+++ src/models/translation-merger.ts
@@ -13,30 +13,36 @@ export class TranslationMerger extends GahPlugin {
   public async onInstall(existingCfg: TranslationManagerConfig): Promise<GahPluginConfig> {
     const newCfg = new TranslationManagerConfig();
 
-    newCfg.searchGlobPattern = await this.promptService.input({
-      msg: 'Please enter a globbing path to the json translation files',
-      default: 'src/assets/**/translations/*.json',
-      enabled: () => !(existingCfg?.searchGlobPattern),
-      validator: (val: string) => val.endsWith('.json')
-    }) ?? existingCfg.searchGlobPattern;
-    newCfg.destinationPath = await this.promptService.input({
-      msg: 'Please enter the destination path for the merged translation files',
-      default: 'src/assets/i18n',
-      enabled: () => !(existingCfg?.destinationPath),
-    }) ?? existingCfg.destinationPath;
+    newCfg.searchGlobPattern =
+      (await this.promptService.input({
+        msg: 'Please enter a globbing path to the json translation files',
+        default: 'src/assets/**/translations/*.json',
+        enabled: () => !existingCfg?.searchGlobPattern,
+        validator: (val: string) => val.endsWith('.json')
+      })) ?? existingCfg.searchGlobPattern;
+    newCfg.destinationPath =
+      (await this.promptService.input({
+        msg: 'Please enter the destination path for the merged translation files',
+        default: 'src/assets/i18n',
+        enabled: () => !existingCfg?.destinationPath
+      })) ?? existingCfg.destinationPath;
     // .*\.(\w+)\.json
-    newCfg.localeRegexPattern = await this.promptService.input({
-      msg: 'Please enter a regex that has one matching group that matches the locale of the filename. leave empty for matching the whole filename'
-        + ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
-      default: '',
-      enabled: () => !(existingCfg?.localeRegexPattern),
-    }) ?? existingCfg.localeRegexPattern;
-    newCfg.prefixRegexPattern = await this.promptService.input({
-      msg: 'Please enter a regex that has one matching group that matches the prefix (of the final translation file) within the filename. leave empty for no prefix'
-        + ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
-      default: '',
-      enabled: () => !(existingCfg?.prefixRegexPattern),
-    }) ?? existingCfg.prefixRegexPattern;
+    newCfg.localeRegexPattern =
+      (await this.promptService.input({
+        msg:
+          'Please enter a regex that has one matching group that matches the locale of the filename. leave empty for matching the whole filename' +
+          ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
+        default: '',
+        enabled: () => !existingCfg?.localeRegexPattern
+      })) ?? existingCfg.localeRegexPattern;
+    newCfg.prefixRegexPattern =
+      (await this.promptService.input({
+        msg:
+          'Please enter a regex that has one matching group that matches the prefix (of the final translation file) within the filename. leave empty for no prefix' +
+          ' See https://github.com/awdware/gah-translation-merger for a detailed documentation',
+        default: '',
+        enabled: () => !existingCfg?.prefixRegexPattern
+      })) ?? existingCfg.prefixRegexPattern;
 
     return newCfg;
   }
@@ -56,7 +62,7 @@ export class TranslationMerger extends GahPlugin {
 
   public onInit() {
     this.registerCommands();
-    this.registerEventListener('AFTER_COPY_ASSETS', async (event) => {
+    this.registerEventListener('AFTER_COPY_ASSETS', async event => {
       if (!event.module?.isHost) {
         return;
       }
@@ -69,12 +75,22 @@ export class TranslationMerger extends GahPlugin {
     this.loggerService.log(`Merging translation files for ${name}`);
 
     const cfg = this.config as TranslationManagerConfig;
-    if (!cfg) { throw new Error('Plugin settings have not been provided.'); }
+    if (!cfg) {
+      throw new Error('Plugin settings have not been provided.');
+    }
 
-    if (!cfg.searchGlobPattern) { throw new Error('Missing Setting: searchGlobPattern'); }
-    if (!cfg.destinationPath) { throw new Error('Missing Setting: destinationPath'); }
+    if (!cfg.searchGlobPattern) {
+      throw new Error('Missing Setting: searchGlobPattern');
+    }
+    if (!cfg.destinationPath) {
+      throw new Error('Missing Setting: destinationPath');
+    }
 
-    const allTranslationFiles = await this.fileSystemService.getFilesFromGlob(`.gah/${cfg.searchGlobPattern}`, ['node_modules'], true);
+    const allTranslationFiles = await this.fileSystemService.getFilesFromGlob(
+      `.gah/${cfg.searchGlobPattern}`,
+      ['node_modules'],
+      true
+    );
 
     this.loggerService.debug(`Found translation files:${allTranslationFiles.join(', ')}`);
 
@@ -88,7 +104,7 @@ export class TranslationMerger extends GahPlugin {
       if (cfg.localeRegexPattern) {
         const localeRegex = new RegExp(cfg.localeRegexPattern);
         const localeMatch = path.basename(file).match(localeRegex);
-        if (!localeMatch || !(localeMatch?.[1])) {
+        if (!localeMatch || !localeMatch?.[1]) {
           throw new Error(`The locale matcher did not find the locale in the filename: ${path.basename(file)}`);
         }
         locale = localeMatch[1];
@@ -96,13 +112,11 @@ export class TranslationMerger extends GahPlugin {
         if (cfg.prefixRegexPattern) {
           const prefixRegex = new RegExp(cfg.prefixRegexPattern);
           const prefixMatch = path.basename(file).match(prefixRegex);
-          if (!prefixMatch || !(prefixMatch?.[1])) {
+          if (!prefixMatch || !prefixMatch?.[1]) {
             throw new Error(`The prefix matcher did not find the prefix in the filename: ${path.basename(file)}`);
           }
           prefix = prefixMatch[1];
-
         }
-
       } else {
         locale = path.basename(file).replace(/\.json$/, '');
       }
@@ -114,6 +128,7 @@ export class TranslationMerger extends GahPlugin {
       if (!trans) {
         trans = new TranslationCollection();
         trans.locale = locale;
+        trans.path = file;
         translationCollection.push(trans);
       }
       const parsedContent = JSON.parse(content);
@@ -123,6 +138,41 @@ export class TranslationMerger extends GahPlugin {
         trans.translations = { ...trans.translations, ...parsedContent };
       }
     }
+
+    const getKeysMismatch = (obj1: object, obj2: object, path: string[] = [], res: string[] = []) => {
+      const keys1 = Object.keys(obj1);
+      const keys2 = Object.keys(obj2);
+      const missingKeys = keys1.filter(x => !keys2.some(y => y === x));
+      res.push(...missingKeys.map(mK => path.join('.') + '.' + mK));
+      keys2.push(...missingKeys);
+      const subObjects1 = keys1.map(k => (obj1 as any)[k]).filter(x => typeof x === 'object');
+      const subObjects2 = keys2
+        .map(k => (obj2 as any)[k])
+        .map(x => (!x ? {} : x))
+        .filter(x => typeof x === 'object');
+
+      for (let i = 0; i < subObjects1.length; i++) {
+        const keyInObj = Object.keys(obj1).find(key => (obj1 as any)[key] === subObjects1[i])!;
+        path.push(keyInObj);
+        getKeysMismatch(subObjects1[i], subObjects2[i], path, res);
+        path.pop();
+      }
+
+      return res;
+    };
+
+    translationCollection.forEach(tC => {
+      translationCollection.forEach(tC2 => {
+        if (tC !== tC2) {
+          const missingKeys = getKeysMismatch(tC.translations, tC2.translations);
+          missingKeys.forEach(missingKey => {
+            this.loggerService.warn(
+              `Translation key ${missingKey} is missing from locale ${tC2.locale} but present in ${tC.path}`
+            );
+          });
+        }
+      });
+    });
 
     await this.fileSystemService.ensureDirectory(destinationPath);
 
